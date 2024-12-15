4f7fcfbdb2c9d27cb847f6125e5836cb822479f8
Te0-faN <simerea.teofan@cnprsv.ro>
2024-12-15 16:20:50 +0200

    \n

--- MeepMeep/src/main/java/MeepMeep/Test/TestGri.java
+++ MeepMeep/src/main/java/MeepMeep/Test/TestGri.java
@@ -6,19 +6,19 @@ import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class TestCuloare
+public class TestGri
 {
     static final float ROBOT_LEN       = 14f;
     static final float TILE_LEN        = 24f;
     static final float BETWEEN_SAMPLES = 10f;
     static final float SAMPLES_WITH    = 3.5f;
     static final float INTAKE          = ROBOT_LEN / 4;
-    static final float OUTAKE          = ROBOT_LEN / 2;
+    static final float OUTTAKE          = ROBOT_LEN / 2;
 
-    static final Pose2d start_pos = new Pose2d(-TILE_LEN + 0.5*ROBOT_LEN, 3*TILE_LEN - 0.5*ROBOT_LEN, Math.toRadians(270));
-    static final Pose2d outpos    = new Pose2d(2*TILE_LEN + OUTAKE, 2.5*TILE_LEN, Math.toRadians(-135));
+    static final Pose2d start_pos = new Pose2d(TILE_LEN - 0.5*ROBOT_LEN, 3*TILE_LEN - 0.5*ROBOT_LEN, Math.toRadians(270));
+    static final Pose2d outpos    = new Pose2d(2*TILE_LEN + OUTTAKE, 2.5*TILE_LEN, Math.toRadians(-135));
 
-    static final Pose2d inpos     = new Pose2d(-2*TILE_LEN, 1.5*TILE_LEN + INTAKE, Math.toRadians(-90));
+    static final Pose2d inpos     = new Pose2d(2*TILE_LEN, 1.5*TILE_LEN + INTAKE, Math.toRadians(-90));
 
     private static float ToInch(float a) {
         return a / 2.54f;
@@ -32,7 +32,7 @@ public class TestCuloare
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), ROBOT_LEN)
                 .setDimensions(ROBOT_LEN, ROBOT_LEN)
                 .followTrajectorySequence((d) -> d.trajectorySequenceBuilder(start_pos)
-                        .splineToLinearHeading(new Pose2d(2*TILE_LEN + OUTAKE, 2.5*TILE_LEN, Math.toRadians(-135)),
+                        .splineToLinearHeading(new Pose2d(2*TILE_LEN + OUTTAKE, 2.5*TILE_LEN, Math.toRadians(-135)),
                                 Math.toRadians(-10))
 
                         /* outake */
@@ -40,15 +40,16 @@ public class TestCuloare
                         /* intake */
                         .lineToSplineHeading(outpos)
                         /* outake */
-                        .lineToSplineHeading(new Pose2d(inpos.getX() - BETWEEN_SAMPLES, inpos.getY(), inpos.getHeading()))
+                        .lineToSplineHeading(new Pose2d(inpos.getX() + BETWEEN_SAMPLES, inpos.getY(), inpos.getHeading()))
                         /* intake */
                         .lineToSplineHeading(outpos)
                         /* outake */
-                        .lineToSplineHeading(new Pose2d(inpos.getX() -(TILE_LEN - 2*BETWEEN_SAMPLES + INTAKE),
-                                                        TILE_LEN + 0.5*SAMPLES_WITH, Math.toRadians(-180)))
+                        .lineToSplineHeading(new Pose2d(inpos.getX() + 2*BETWEEN_SAMPLES - OUTTAKE - INTAKE,
+                                                        TILE_LEN + 0.5*SAMPLES_WITH, Math.toRadians(0)))
                         /* intake */
                         .lineToSplineHeading(outpos)
                         /* outake */
+
                         .build());
 
 
